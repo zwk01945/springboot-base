4b2b4748c0412c33fca7eae844884567aac29b7c
zwk <785915202@qq.com>
2020-07-07 16:59:17 +0800

    map

---
+++ map/src/main/java/com/example/map/MapApplication.java
@@ -0,0 +1,14 @@
+package com.example.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MapApplication.class, args);
+    }
+
+
+}
